581e9f74dc747e324ffa508bf6a2974b01ad94b9
15017519 <15017519@myrp.edu.sg>
2017-05-05 14:01:53 +0800

    add data

---
+++ app/src/main/java/sg/edu/rp/c346/p03_classjournal/AddData.java
@@ -0,0 +1,49 @@
+package sg.edu.rp.c346.p03_classjournal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class AddData extends AppCompatActivity {
+
+    Button btnSubmit;
+    RadioGroup rgGrade;
+    TextView tvWeek;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_add_data);
+
+        btnSubmit = (Button)findViewById(R.id.buttonSubmit);
+        rgGrade = (RadioGroup)findViewById(R.id.radiogroupGrade);
+        tvWeek = (TextView)findViewById(R.id.textViewWeek);
+        int selectedButtonId = rgGrade.getCheckedRadioButtonId();
+        final RadioButton rbGrade = (RadioButton) findViewById(selectedButtonId);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String grade = rbGrade.getText().toString();
+                int week = Integer.parseInt(tvWeek.getText().toString());
+                Intent intent = getIntent();
+                String moduleCode = intent.getStringExtra("module code");
+            }
+        });
+
+
+
+
+
+
+    }
+
+}
